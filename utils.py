5c04c9da6c316f777e6d40f4f3fa4e19cfac556d
Amstf <ali_mustapha_@outlook.com>
2022-08-25 14:32:28 +0200

    some update at files (utils)

---
+++ utils.py
@@ -0,0 +1,50 @@
+# import libraries
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+def drop_d(df,features):
+    list_1=list(features["col_name"][:20])
+    for i in(list_1):
+        df=df.drop(i,axis=1)
+    return df
+
+def get_real_data(data_path):
+    df=pd.read_csv(data_path)
+    df=df.loc[df["Label"]==1]
+    df=df.iloc[:8000]
+    df.drop("Label",axis=1)
+    df["Label"]=0
+    return df
+
+def get_fake_data(data_path):
+    df=pd.read_csv(data_path)
+    df["Label"]=1
+    return df
+
+def get_combined_data(real_df,fake_df):
+    return pd.concat([real_df, fake_df], axis=0)
+def data_split(df):
+    
+    y=df.Label
+    X=df.drop(columns=["Label"])
+    # X=df[columns]
+    
+    labels=y.unique()
+    classes=y.nunique()
+
+
+    print(X.shape)
+    print("number of Label", classes)
+    print("instances per label\n", y.value_counts())
+    print("label",labels)
+    
+    # split the dataset into 80% for training and 20% for testing
+    X_train , X_test, y_train , y_test = train_test_split(X,y, random_state=42 , stratify=y, shuffle=True,test_size=0.2)
+
+
+    print("after spliting the data :\n")
+    print("training data length:", len(X_train))
+    print("test data length:", len(X_test))
+
+    return X_train , X_test, y_train , y_test
