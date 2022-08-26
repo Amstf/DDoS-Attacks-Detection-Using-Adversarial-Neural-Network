23e363f25e0fd3d8a5665dd6c5bfc277a0f4abbe
Amstf <ali_mustapha_@outlook.com>
2022-08-26 13:29:59 +0200

    Add load model function

--- cicflowmeter/src/cicflowmeter/flow_session.py
+++ cicflowmeter/src/cicflowmeter/flow_session.py
@@ -1,28 +1,34 @@
 import csv
 from collections import defaultdict
 
+import requests
 from scapy.sessions import DefaultSession
-
+import threading
+from queue import Queue
 from .features.context.packet_direction import PacketDirection
 from .features.context.packet_flow_key import get_packet_flow_key
 from .flow import Flow
+import pandas as pd
+import tensorflow as tf
+
 
 EXPIRED_UPDATE = 40
 MACHINE_LEARNING_API = "http://localhost:8000/predict"
 GARBAGE_COLLECT_PACKETS = 100
-ff=0
 
 class FlowSession(DefaultSession):
     """Creates a list of network flows."""
-
+    
     def __init__(self, *args, **kwargs):
         self.flows = {}
         self.csv_line = 0
 
+
+
+
         if self.output_mode == "flow":
             output = open(self.output_file, "w")
             self.csv_writer = csv.writer(output)
-            print("output", output)
 
         self.packets_count = 0
 
@@ -62,18 +68,31 @@ class FlowSession(DefaultSession):
             packet_flow_key = get_packet_flow_key(packet, direction)
             flow = self.flows.get((packet_flow_key, count))
 
-        if flow is None:
-            # If no flow exists create a new flow
-            direction = PacketDirection.FORWARD
-            flow = Flow(packet, direction)
-            packet_flow_key = get_packet_flow_key(packet, direction)
-            self.flows[(packet_flow_key, count)] = flow
+            if flow is None:
+                # If no flow exists create a new flow
+                direction = PacketDirection.FORWARD
+                flow = Flow(packet, direction)
+                packet_flow_key = get_packet_flow_key(packet, direction)
+                self.flows[(packet_flow_key, count)] = flow
+
+            elif (packet.time - flow.latest_timestamp) > EXPIRED_UPDATE:
+                # If the packet exists in the flow but the packet is sent
+                # after too much of a delay than it is a part of a new flow.
+                expired = EXPIRED_UPDATE
+                while (packet.time - flow.latest_timestamp) > expired:
+                    count += 1
+                    expired += EXPIRED_UPDATE
+                    flow = self.flows.get((packet_flow_key, count))
+
+                    if flow is None:
+                        flow = Flow(packet, direction)
+                        self.flows[(packet_flow_key, count)] = flow
+                        break
 
         elif (packet.time - flow.latest_timestamp) > EXPIRED_UPDATE:
-            # If the packet exists in the flow but the packet is sent
-            # after too much of a delay than it is a part of a new flow.
             expired = EXPIRED_UPDATE
             while (packet.time - flow.latest_timestamp) > expired:
+
                 count += 1
                 expired += EXPIRED_UPDATE
                 flow = self.flows.get((packet_flow_key, count))
@@ -82,11 +101,6 @@ class FlowSession(DefaultSession):
                     flow = Flow(packet, direction)
                     self.flows[(packet_flow_key, count)] = flow
                     break
-        elif "F" in str(packet.flags):
-            # If it has FIN flag then early collect flow and continue
-            flow.add_packet(packet, direction)
-            self.garbage_collect(packet.time)
-            return
 
         flow.add_packet(packet, direction)
 
@@ -101,8 +115,12 @@ class FlowSession(DefaultSession):
     def get_flows(self) -> list:
         return self.flows.values()
 
+
+        
     def garbage_collect(self, latest_time) -> None:
         # TODO: Garbage Collection / Feature Extraction should have a separate thread
+        model= load_model('model-best.h5')
+        print(model.summary())
         if not self.url_model:
             print("Garbage Collection Began. Flows = {}".format(len(self.flows)))
         keys = list(self.flows.keys())
@@ -116,14 +134,45 @@ class FlowSession(DefaultSession):
             ):
                 data = flow.get_data()
 
+                # POST Request to Model API
+                if self.url_model:
+                    payload = {
+                        "columns": list(data.keys()),
+                        "data": [list(data.values())],
+                    }
+                    post = requests.post(
+                        self.url_model,
+                        json=payload,
+                        headers={
+                            "Content-Type": "application/json; format=pandas-split"
+                        },
+                    )
+                    resp = post.json()
+                    result = resp["result"].pop
+                    if result == 0:
+                        result_print = "Benign"
+                    else:
+                        result_print = "Malicious"
+
+                    print(
+                        "{: <15}:{: <6} -> {: <15}:{: <6} \t {} (~{:.2f}%)".format(
+                            resp["src_ip"],
+                            resp["src_port"],
+                            resp["dst_ip"],
+                            resp["dst_port"],
+                            result_print,
+                            resp["probability"].pop()[result] * 100,
+                        )
+                    )
+                keys= [' Min Packet Length', 'Init_Win_bytes_forward', ' Fwd Packet Length Min', ' Destination Port', ' min_seg_size_forward', ' Average Packet Size', ' Init_Win_bytes_backward', ' Total Fwd Packets', 'Flow Bytes/s', ' Flow IAT Min', ' Packet Length Mean', 'Subflow Fwd Packets', ' Bwd Packets/s', 'Bwd IAT Total', ' Max Packet Length', ' Bwd IAT Max', ' Subflow Bwd Packets', ' Bwd Header Length', ' Fwd Packet Length Mean', ' Fwd Header Length']
+                data = {x:data[x] for x in keys}
+                # print(data.values())
                 if self.csv_line == 0:
                     self.csv_writer.writerow(data.keys())
-                   
+                display(data)
 
                 self.csv_writer.writerow(data.values())
-                print("Dada",data.values())
                 self.csv_line += 1
-              
 
                 del self.flows[k]
         if not self.url_model:
@@ -140,3 +189,10 @@ def generate_session_class(output_mode, output_file, url_model):
             "url_model": url_model,
         },
     )
+def load_model(model_path):
+    model = tf.keras.models.load_model(model_path)
+    model.compile(loss="sparse_categorical_crossentropy", optimizer='Adam',metrics=['accuracy'])
+    return model
+
+def display(data):
+    print(data)
