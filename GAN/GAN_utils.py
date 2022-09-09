fcdfec014d96f8f203709ae43aa4429eddc13f06
Amstf <ali_mustapha_@outlook.com>
2022-09-09 10:36:40 +0200

    add the gradoemt calculation function(penalty)

--- GAN/GAN_utils.py
+++ GAN/GAN_utils.py
@@ -74,3 +74,24 @@ def prepare_data(df, batch_size):
     train_ds = TensorDataset(data)
     train_dl = DataLoader(train_ds, batch_size = batch_size, drop_last=True)
     return ohe, scaler, input_dim, discrete_columns, continuous_columns ,train_dl, data_train, data_test
+
+def get_gradient(crit, real, fake, epsilon):
+    mixed_data = real * epsilon + fake * (1 - epsilon)
+
+    mixed_scores = crit(mixed_data)
+
+    gradient = torch.autograd.grad(
+        inputs=mixed_data,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    return gradient
+
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+    gradient_norm = gradient.norm(2, dim=1)
+
+    penalty = torch.mean((gradient_norm - 1) ** 2)
+    return penalty
