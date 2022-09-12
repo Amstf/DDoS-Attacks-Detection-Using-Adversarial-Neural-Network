8954c0bff01c8f10181f891023fe4ee01c5c2cea
Amstf <ali_mustapha_@outlook.com>
2022-09-12 13:19:39 +0200

    add the generator and the critic loss

--- GAN/GAN_utils.py
+++ GAN/GAN_utils.py
@@ -95,3 +95,14 @@ def get_gradient_penalty(gradient):
 
     penalty = torch.mean((gradient_norm - 1) ** 2)
     return penalty
+
+def get_gen_loss(crit_fake_pred):
+    gen_loss = -1. * torch.mean(crit_fake_pred)
+
+    return gen_loss
+
+
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred) + c_lambda * gp
+
+    return crit_loss
