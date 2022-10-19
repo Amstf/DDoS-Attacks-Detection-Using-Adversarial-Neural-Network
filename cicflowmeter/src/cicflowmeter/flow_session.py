15729dbd12f9a8c9b63108efad272bf323b7774d
Amstf <ali_mustapha_@outlook.com>
2022-10-19 12:11:22 +0200

    add the get label function

--- cicflowmeter/src/cicflowmeter/flow_session.py
+++ cicflowmeter/src/cicflowmeter/flow_session.py
@@ -116,7 +116,7 @@ class FlowSession(DefaultSession):
 
     def get_flows(self) -> list:
         return self.flows.values()
-    def gfg(self,model,data_queue):
+    def get_label(self,model,data_queue):
         keys=[' Destination Port', ' Flow Duration', ' Total Fwd Packets', ' Total Backward Packets', 'Total Length of Fwd Packets', ' Total Length of Bwd Packets', ' Fwd Packet Length Max', ' Fwd Packet Length Min', ' Fwd Packet Length Mean', ' Fwd Packet Length Std', 'Bwd Packet Length Max', ' Bwd Packet Length Mean', ' Bwd Packet Length Std', 'Flow Bytes/s', ' Flow Packets/s', ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min', 'Fwd IAT Total', ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Max', ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean', ' Bwd IAT Std', ' Bwd IAT Max', ' Bwd IAT Min', ' Fwd Header Length', ' Bwd Header Length', 'Fwd Packets/s', ' Bwd Packets/s', ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean', ' Packet Length Std', ' Packet Length Variance', ' Down/Up Ratio', ' Average Packet Size', ' Avg Fwd Segment Size', ' Avg Bwd Segment Size', 'Subflow Fwd Packets', ' Subflow Fwd Bytes', ' Subflow Bwd Packets', ' Subflow Bwd Bytes', 'Init_Win_bytes_forward', ' Init_Win_bytes_backward', ' act_data_pkt_fwd', ' min_seg_size_forward', 'Active Mean', ' Active Std', ' Active Max', ' Active Min', 'Idle Mean', ' Idle Std', ' Idle Max', ' Idle Min']
         for i in range(5):
             data=data_queue.get()
@@ -210,7 +210,7 @@ class FlowSession(DefaultSession):
 
                 data_queue.put(data)
                 if (data_queue.qsize()>=5):
-                    timer = threading.Timer(2.0, self.gfg,args=(model,data_queue,))
+                    timer = threading.Timer(2.0, self.get_label,args=(model,data_queue,))
                     timer.start()                    
                 else:
                     print("size of queue = ",data_queue.qsize())
